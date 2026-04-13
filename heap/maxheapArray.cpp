b8089e1d32486bc016b3924f66a779b627510912
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-04-13 21:46:12 +0530

    max heap using array

---
+++ heap/maxheapArray.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class MaxHeap {
+    vector<int> heap;
+
+    void heapifyUp(int index) {
+        if (index == 0) return;
+        int parent = (index - 1) / 2;
+        if (heap[index] > heap[parent]) {
+            swap(heap[index], heap[parent]);
+            heapifyUp(parent);
+        }
+    }
+
+    void heapifyDown(int index) {
+        int largest = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if (left < heap.size() && heap[left] > heap[largest])
+            largest = left;
+        if (right < heap.size() && heap[right] > heap[largest])
+            largest = right;
+
+        if (largest != index) {
+            swap(heap[index], heap[largest]);
+            heapifyDown(largest);
+        }
+    }
+
+public:
+    void insert(int val) {
+        heap.push_back(val);
+        heapifyUp(heap.size() - 1);
+    }
+
+    void display() {
+        for (int val : heap) cout << val << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    MaxHeap mh;
+    mh.insert(10);
+    mh.insert(20);
+    mh.insert(5);
+    mh.insert(30);
+
+    cout << "Max Heap array: ";
+    mh.display();
+
+    return 0;
+}
