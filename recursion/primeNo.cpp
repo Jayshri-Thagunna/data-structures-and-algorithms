a71fb4d8ea785f1a6cb35387b9a628250b63f65a
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-04-13 22:02:00 +0530

    prime no using recursion

---
+++ recursion/primeNo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n, int i = 2) {
+    // Base cases
+    if (n <= 2) 
+        return (n == 2) ? true : false;
+    if (n % i == 0) 
+        return false;
+    if (i * i > n) 
+        return true;
+
+    // Recursive call
+    return isPrime(n, i + 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (isPrime(n))
+        cout << n << " is a prime number." << endl;
+    else
+        cout << n << " is not a prime number." << endl;
+
+    return 0;
+}
