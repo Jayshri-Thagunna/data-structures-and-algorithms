fa80ee0f14ff73a5d746270b904f0481fbbc8815
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-04-13 21:36:43 +0530

    create and insert at bottom on doubly linkedList

---
+++ doublylinkedList/create.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int val) : data(val), next(nullptr), prev(nullptr) {}
+};
+
+void insertAtBottom(Node* &head, int value) {
+    Node* newNode = new Node(value);
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = nullptr;
+    insertAtBottom(head, 6);
+    insertAtBottom(head, 5);
+    insertAtBottom(head, 4);
+    display(head);
+    
+    insertAtBottom(head, 10);
+    cout << "List after insertion at bottom: ";
+    display(head);
+    
+    return 0;
+}
