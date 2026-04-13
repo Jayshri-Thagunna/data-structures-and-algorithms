2f2fc44094d1f304a7d03c54afc8ef16925f3def
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-04-13 21:58:55 +0530

    fibonacci number using recursion

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    // Base case
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter position: ";
+    cin >> n;
+
+    cout << "Fibonacci number at position " << n << " is: " << fibonacci(n);
+
+    return 0;
+}
