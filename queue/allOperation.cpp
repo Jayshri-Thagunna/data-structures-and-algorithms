1c1286425fa7b866f268d685119038c9188d69f8
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-05-20 06:44:23 +0530

    peek

--- queue/allOperation.cpp
+++ queue/allOperation.cpp
@@ -36,6 +36,32 @@ class Queue {
         cout << x <<" enqueued into queue" << endl;
     }
 
+    //remove an element from the front
+    int dequeue() {
+        if(front == NULL){
+            cout << "Queue is empty."<<endl;
+            return -1;
+        }
+        Node *temp = front;
+        int dequeuedValue = temp->data;
+        front = front->next;
+
+        if(front == NULL){
+            rear = NULL;
+        }
+        delete temp;
+
+        return dequeuedValue;
+    }
+
+    //front element
+    int peek(){
+        if(front == NULL){
+            cout << " Queue is Empty" << endl;
+            return -1;
+        }
+        return front ->data;
+    }
 };
 
 int main() {
@@ -44,4 +70,9 @@ int main() {
     q.enqueue(10);
     q.enqueue(20);
     q.enqueue(30);
+
+    cout << q.dequeue() <<" dequed from queue" << endl;
+    cout << "Front element is: "<< q.peek()<<endl;
+
+    return 0;
 }
